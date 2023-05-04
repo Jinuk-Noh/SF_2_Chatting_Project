daab3701d084e3210a5a821677d5afe95cb665b9
sujung00061 <sujung00061@naver.com>
2023-05-04 20:34:08 +0900

    feat: signup

--- ChattingProject/ChattingClient/ChattingClient.cpp
+++ ChattingProject/ChattingClient/ChattingClient.cpp
@@ -1,22 +1,19 @@
 #pragma comment(lib, "ws2_32.lib")
 #include <WinSock2.h> //Winsock 헤더파일 include. WSADATA 들어있음
 #include <WS2tcpip.h>
-
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <thread>
 #include <vector>
-
 #define MAX_SIZE 1024
-
 using namespace std;
-
 SOCKET client_sock;
-
+int CheckIdInfo();
 int main() {
+	string id, pw, pw_1, name;
+	cout << "회원가입" << endl << "사용할 ID를 입력해주십시오: ";
+	cin >> id;
+	cout << endl;
 
 }
-
-
-
