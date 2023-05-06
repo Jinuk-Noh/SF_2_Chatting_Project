ccae030c7439282d386aa2d000772fbf9993ed44
sujung00061 <sujung00061@naver.com>
2023-05-06 09:17:36 +0900

    깨진 문자 수정

--- ChattingProject/ChattingClient/ChattingClient.cpp
+++ ChattingProject/ChattingClient/ChattingClient.cpp
@@ -1,5 +1,5 @@
 #pragma comment(lib, "ws2_32.lib")
-#include <WinSock2.h> //Winsock ������� include. WSADATA �������
+#include <WinSock2.h> //Winsock 헤더파일 include. WSADATA 들어있음
 #include <WS2tcpip.h>
 #include <iostream>
 #include <string>
@@ -17,11 +17,11 @@ int CheckIdInfo();
 int main() {
 	while (1) {
 		cout << "------------------------------" << endl;
-		cout << "1 : �α���" << endl;
-		cout << "2 : ȸ������" << endl;
-		cout << "3 : ä�ù� ����" << endl;
+		cout << "1 : 로그인" << endl;
+		cout << "2 : 회원가입" << endl;
+		cout << "3 : 채팅 들어가기" << endl;
 		cout << "------------------------------" << endl;
-		cout << "�Է� : ";
+		cout << "입력 : ";
 
 		int commNum = stoi(GetCommNum());
 
@@ -33,7 +33,7 @@ int main() {
 		case 2:
 			break;
 		case 3:
-			if (userInfo.id == "") cout << "�α��� ���� �ʾҽ��ϴ�." << endl;
+			if (userInfo.id == "") cout << "로그인 해 주십시오." << endl;
 			Chatting();
 			break;
 		default:
@@ -47,7 +47,7 @@ string GetCommNum() {
 	do {
 		cin >> input;
 		if (input != "1" && input != "2" && input != "3") {
-			cout << "1 ~ 3 �� �Է��ϼ��� : ";
+			cout << "1 ~ 3 이내의 수를 입력해주십시오 : ";
 		}
 	} while (input != "1" 
 		&& input != "2"
